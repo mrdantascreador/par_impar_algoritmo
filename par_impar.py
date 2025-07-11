7ce86aac9c67505d74fbeeced2c335e6d36e4f1c
mrdantascreador <kaiocesar9977@gmail.com>
2025-07-11 13:34:49 +0000

    code de par e impar

---
+++ par_impar.py
@@ -0,0 +1,14 @@
+numero = 0 
+
+while numero <= 20: 
+    if numero %2 == 0: 
+        print('O número par é: ')
+        print(numero)
+
+
+    elif numero %2 != 0: 
+        print('O número ímpar é: ')
+        print(numero)
+
+
+    numero = numero + 1 
